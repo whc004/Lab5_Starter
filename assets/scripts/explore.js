0263532ee3e2a2f5b40b4e79b2993cb77128605b
whc004 <whc004@ucsd.edu>
2024-05-07 21:21:13 -0700

    finish javascript

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,52 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  const talkButton = document.querySelector('button');
+  talkButton.addEventListener('click', pressPlay);
+}
+
+function pressPlay() {
+  var textareaElement = document.getElementById('text-to-speak');
+  var text = textareaElement.value;
+  //console.log("Text in textarea: ", text);
+
+  const voiceSelect = document.getElementById('voice-select');
+  const selectedOption = voiceSelect.selectedOptions[0];
+  const selectedName = selectedOption.getAttribute('data-name');
+  const selectedLang = selectedOption.getAttribute('data-lang');
+  //console.log("Selected voice: ", selectedName, selectedLang);
+
+
+  const speaker = new SpeechSynthesisUtterance(text);
+  const voices = speechSynthesis.getVoices();
+
+  speaker.voice = voices.find(voice => voice.name === selectedName && voice.lang === selectedLang);
+  const img = document.querySelector('img');
+  speaker.onstart = () => {
+    //console.log("Speech has started");
+    img.src = 'assets/images/smiling-open.png';
+  };
+  speaker.onend = () => {
+    //console.log("Speech has ended");
+    img.src = 'assets/images/smiling.png';
+  };
+  speechSynthesis.speak(speaker);
+}
+
+function populateVoiceList() {
+  const voiceSelect = document.getElementById('voice-select');
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const voice = voices[i];
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+  };
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,61 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const horn = document.getElementById('horn-select');
+  const volume = document.getElementById('volume');
+  const play = document.querySelector('button');
+
+  horn.addEventListener('change', updateHorn);
+  volume.addEventListener('input', updateVolume);
+  play.addEventListener('click', playSound);
 }
+
+function updateHorn() {
+  const hornSelect = document.getElementById('horn-select');
+  const hornType = hornSelect.value;
+
+  const imgElement = document.querySelector('#expose img');
+  const audioElement = document.querySelector('audio');
+  imgElement.src = `assets/images/${hornType}.svg`;
+  audioElement.src = `assets/audio/${hornType}.mp3`;
+
+}
+
+function updateVolume() {
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const audioElement = document.querySelector('audio');
+
+  const volume = volumeSlider.value;
+  audioElement.volume = volume / 100;
+
+  console.log("volume src: ", volume);
+  console.log("audioElement.volume src: ", audioElement.volume);
+  if (volume == 0) {
+    volumeIcon.src = 'assets/icons/volume-level-0.svg';
+  } else if (volume < 33) {
+    volumeIcon.src = 'assets/icons/volume-level-1.svg';
+  } else if (volume < 67) {
+    volumeIcon.src = 'assets/icons/volume-level-2.svg';
+  } else if (volume <= 100) {
+    volumeIcon.src = 'assets/icons/volume-level-3.svg';
+  }
+  else {
+    console.log("Error")
+  }
+}
+function playSound() {
+  const audioElement = document.querySelector('audio');
+  const hornSelect = document.getElementById('horn-select');
+  audioElement.play()
+  if (hornSelect.value === 'party-horn') {
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+      confettiRadius: 10,
+      confettiNumber: 1000,
+      origin: { x: 0.5, y: 0.5 }
+    });
+  }
+}
+
