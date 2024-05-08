71084b90a192d0b3a54642da74432ea251ee8759
whc004 <whc004@ucsd.edu>
2024-05-07 22:20:11 -0700

    [Insert message]

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,8 +1,8 @@
 // sum.test.js
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).(3);
+  expect(1 + 2).toBe(3);
 });
 import { sum } from '../code-to-unit-test/sum';
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).(3);
+  expect(sum(1, 2)).toBe(3);
 });
