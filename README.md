92ab359bf953aca645139a9f8882709eeaf2bebc
whc004 <whc004@ucsd.edu>
2024-05-07 22:10:32 -0700

    jtest

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lab 5 - Starter
 Wen Hsin Chang
+## 1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+### Since it have the inteaction between the user and the message reciever which involve multiple system components and external dependencies, I won't choose to use a unit test to test it.
+
+## 2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+### Since it only test the length of the max message length which is an isolated features, I would choose to use a unit test to test it.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,68 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('isPhoneNumber', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('isPhoneNumber', () => {
+  expect(isPhoneNumber('321-456-7890')).toBe(true);
+});
+test('isPhoneNumber', () => {
+  expect(isPhoneNumber('12356sad7890aaa')).toBe(false);
+});
+test('isPhoneNumber', () => {
+  expect(isPhoneNumber('12ddd34567890')).toBe(false);
+});
+
+test('isEmail', () => {
+  expect(isEmail('example@gmail.com')).toBe(true);
+});
+test('isEmail', () => {
+  expect(isEmail('example@ucsd.edu')).toBe(true);
+});
+test('isEmail', () => {
+  expect(isEmail('example@......sssss')).toBe(false);
+});
+test('isEmail', () => {
+  expect(isEmail('example!!!ucsd.edu......')).toBe(false);
+});
+
+
+test('isStrongPassword', () => {
+  expect(isStrongPassword('a_11')).toBe(true);
+});
+test('isStrongPassword', () => {
+  expect(isStrongPassword('c_123')).toBe(true);
+});
+test('isStrongPassword', () => {
+  expect(isStrongPassword('12345678')).toBe(false);
+});
+test('isStrongPassword', () => {
+  expect(isStrongPassword('87654321')).toBe(false);
+});
+
+test('isDate', () => {
+  expect(isDate('01/01/2021')).toBe(true);
+});
+test('isDate', () => {
+  expect(isDate('01/01/2022')).toBe(true);
+});
+test('isDate', () => {
+  expect(isDate('2020/21/01')).toBe(false);
+});
+test('isDate', () => {
+  expect(isDate('2020/11/01')).toBe(false);
+});
+
+test('isHexColor', () => {
+  expect(isHexColor('#123ABC')).toBe(true);
+});
+test('isHexColor', () => {
+  expect(isHexColor('#ABC123')).toBe(true);
+});
+test('isHexColor', () => {
+  expect(isHexColor('11111')).toBe(false);
+});
+test('isHexColor', () => {
+  expect(isHexColor('#XXX')).toBe(false);
+});
